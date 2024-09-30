88d46e2c0f6cff95ae643c0d74517f74fb392327
joao <jpvicente2508@gmail.com>
2024-09-30 15:36:04 +0100

    improvements for mobile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/thank-you',
+        destination: '/thank-you',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
