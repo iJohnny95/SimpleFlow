cd9523fdef14dd711da3d9ca6b660c07d5fc2595
joao <jpvicente2508@gmail.com>
2024-09-30 15:55:32 +0100

    form submission

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/thank-you',
-        destination: '/thank-you',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
