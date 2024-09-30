a3297cbeb94a80564283d771d7a2922ce192013c
joao <jpvicente2508@gmail.com>
2024-09-30 12:52:56 +0100

    pricing list updated for mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        tickBlue: '#2DD4BF', // Add the baby blue color here
       },
     },
   },
